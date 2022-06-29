b5d40e95bd933e116063ce6d562ae368d1b3bc39
iamardian <ardian.zanshir@gmail.com>
2022-06-29 11:00:05 +0430

    Update : fix device

--- ecgan_parsbert.py
+++ ecgan_parsbert.py
@@ -855,7 +855,7 @@ def per_label_accuracy(b_labels, predicted, class_accuracies):
         label_indexes = torch.where((b_labels == label))[0]
         number_of_label = len(label_indexes)
         predicts = [predicted[i].item() for i in label_indexes]
-        true_predicts = torch.sum((torch.tensor(predicts) == label)).item()
+        true_predicts = torch.sum((torch.tensor(predicts).to(device) == label)).item()
         # print(f"predicts : {predicts}")
         # print(f"label : {label}")
         # print(f"true_predicts : {true_predicts}")
