e0f638925a2c1d452fa6afb628cf3ba23fc3a467
iamardian <ardian.zanshir@gmail.com>
2022-06-29 20:51:16 +0430

    Update : print len df

--- ecgan_parsbert.py
+++ ecgan_parsbert.py
@@ -1061,6 +1061,7 @@ def print_results(train_acc, validation_acc, test_acc):
         height = 360
         for i in range(len(df)):
             for j in range(len(df.iloc[i])):
+                print("len : ",len(df.iloc[i]))
                 print(f"{i},{j}")
                 print(df.iloc[i, j])
                 worksheet.write(i, j, df.iloc[i, j])
