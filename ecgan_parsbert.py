41e539db012a6e1366c4d7dc68bb25332e3b1a90
iamardian <ardian.zanshir@gmail.com>
2022-07-04 17:52:02 +0430

    Update : fix print to file

--- ecgan_parsbert.py
+++ ecgan_parsbert.py
@@ -153,12 +153,12 @@ create_path_if_not_exists(models_path)
 
 
 def log_print(*args):
-    print()
     source_file = open(models_path+"/log_file.txt", 'a')
+    print()
+    print("\n", file=source_file)
     for a in args:
         print(a, end=' ')
         print(a, file=source_file, end=' ')
-    print("\n", file=source_file)
     source_file.close()
 
 
