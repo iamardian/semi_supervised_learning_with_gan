2d920e54438015f62b84b56da81a3d30d617be0e
iamardian <ardian.zanshir@gmail.com>
2022-07-06 17:53:06 +0430

    Update : remove save and load scheduler

--- ecgan_parsbert.py
+++ ecgan_parsbert.py
@@ -808,9 +808,9 @@ def load_params(load_path, classifier, generator, discriminator, transformer, cf
     total_label_base_accuracy_evaluation = checkpoint['total_label_base_accuracy_evaluation']
     total_label_base_accuracy_test = checkpoint['total_label_base_accuracy_test']
 
-    dis_scheduler.load_state_dict(checkpoint['dis_scheduler'])
-    cfr_scheduler.load_state_dict(checkpoint['cfr_scheduler'])
-    gen_scheduler.load_state_dict(checkpoint['gen_scheduler'])
+    # dis_scheduler.load_state_dict(checkpoint['dis_scheduler'])
+    # cfr_scheduler.load_state_dict(checkpoint['cfr_scheduler'])
+    # gen_scheduler.load_state_dict(checkpoint['gen_scheduler'])
 
 
 def save_params(epoch, save_path):
@@ -843,9 +843,9 @@ def save_params(epoch, save_path):
             'total_label_base_accuracy_validation': total_label_base_accuracy_validation,
             'total_label_base_accuracy_evaluation': total_label_base_accuracy_evaluation,
             'total_label_base_accuracy_test': total_label_base_accuracy_test,
-            'dis_scheduler': dis_scheduler.state_dict(),
-            'cfr_scheduler': cfr_scheduler.state_dict(),
-            'gen_scheduler': gen_scheduler.state_dict(),
+            # 'dis_scheduler': dis_scheduler.state_dict(),
+            # 'cfr_scheduler': cfr_scheduler.state_dict(),
+            # 'gen_scheduler': gen_scheduler.state_dict(),
         }, model_path_name)
         remove_previous_models(save_path, model_name_path)
     except Exception as e:
